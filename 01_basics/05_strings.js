e296346352c4ad547d303ea0be01625efc7f47bb
laxmantrades <laxmanvatij@gmail.com>
2024-08-08 18:53:15 +0000

    DOne

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,36 @@
+// let name = "laxman"
+// let address = "Tulsipur"
+
+// console.log(`My name is ${name} and I live in" `); 
+
+let myname = "LAXMAN";
+// console.log(typeof myname);
+// console.log(myname[3])
+// console.log(myname.fontcolor("red"));
+// console.log(myname.toUpperCase());
+// console.log(myname.toLowerCase() + myname.toUpperCase())
+// console.log(myname.trim());
+// console.log(myname.replace())
+// console.log(myname.substring(0,4))
+
+// let part1 = myname.slice(0, 2);
+
+// console.log(part1);
+
+// let myname = "LAXMAN";
+
+// Extracting a portion of the string
+let part1 = myname.slice(0, 3);   // Extracts "LAX"
+// let part2 = myname.slice(3);      // Extracts "MAN"
+
+// // Display the results
+console.log(part1); // Output: "LAX"
+ console.log(part2); // Output: "MAN"
+
+
+
+
+
+
+
+
